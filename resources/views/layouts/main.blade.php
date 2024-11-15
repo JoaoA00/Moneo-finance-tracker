a30a2fb3b9dc574b0613d004ed40d60639ce5819
João Abner <joaoabner2022@gmail.com>
2024-11-14 21:48:55 -0300

    Criada a estrutura do dashboard
    Criado um layout do dashboard principal, além de criado um botão de logout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="header-pages">
+                <ul>
+                    <li><a href="#">Transações</a></li>
+                    <li><a href="#">Relatório</a></li>
+                    <li><a href="#">Recorrências</a></li>
+                    <li><a href="#">Investimentos</a></li>
+                    <li><a href="#">Tutorial</a></li>
+                </ul>
+            </div>
+            <div class="right-itens">
+                <div class="user-profile">
+                    <a href="">
+                        <img src="/img/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="logout">
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <a href="/logout" class="nav-link" 
+                            onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                            Sair
+                            <ion-icon name="arrow-forward-outline"></ion-icon>
+                        </a>
+                        
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="content">
+            @yield('content')
+        </div>
+    </main>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
