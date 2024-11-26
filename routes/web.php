58c77808f21e4bca6e8bb82b3276c6f62fbbb0b5
João Abner <joaoabner2022@gmail.com>
2024-11-26 14:25:06 -0300

    Mover o método show para o TransactionController

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use App\Http\Controllers\TransactionsController;
 use App\Http\Controllers\UserController;
+use App\Models\Transaction;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'index']);
 Route::get('/dashboard', [UserController::class, 'dashboard'])->middleware('auth');
 Route::get('/profile', [UserController::class, 'profile'])->middleware('auth');
-Route::get('/transactions', [UserController::class, 'transactions'])->middleware('auth');
+Route::get('/transactions', [TransactionsController::class, 'show'])->middleware('auth');
 Route::get('/add-transaction', [UserController::class, 'addTransaction'])->middleware('auth');
 Route::post('/add-transaction', [TransactionsController::class, 'store'])->middleware('auth');
 
