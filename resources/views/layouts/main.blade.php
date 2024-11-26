225c96119e042fca68dd24118edb32582e7860d5
João Abner <joaoabner2022@gmail.com>
2024-11-26 14:26:46 -0300

    Linkar a table de transactions com o bd e alterar o estilo da página usando bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,44 +8,57 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     
+    <!-- bootstrap-icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>-->
 
     <title>@yield('title')</title>
 </head>
 <body>
     <header>
-        <nav>
-            <div class="header-pages">
-                <ul>
-                    <li><a href="/dashboard">Dashboard</a></li>
-                    <li><a href="/transactions">Transações</a></li>
-                    <li><a href="#">Relatório</a></li>
-                    <li><a href="#">Recorrências</a></li>
-                    <li><a href="#">Investimentos</a></li>
-                    <li><a href="#">Tutorial</a></li>
-                </ul>
-            </div>
-            <div class="right-itens">
-                <div class="user-profile">
-                    <a href="/profile">
-                        <img src="/img/logo.png" alt="">
-                    </a>
-                </div>
-                <div class="logout">
+        <nav class="navbar navbar-expand-md bg-dark">
+            <div class="container-fluid">
+                <a href="/" class="navbar-brand"><img src="/img/logo-no-bg.png" width="40" height="40"></a>
+
+                <div class="ms-0 d-flex align-items-center order-md-3">
+                    
+                    <button class="navbar-toggler p-0 me-3" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-links">
+                        <span class="text-light"><i class="bi bi-list"></i></span>
+                    </button>
+                    
+                    <div class="navbar-collapse collapse navbar-links">
+                        <a href="/profile">
+                            <img src="/img/logo.png" class="rounded-circle" width="40" height="40" alt="">
+                        </a>
+                    </div>
+                    
                     <form action="/logout" method="POST">
                         @csrf
-                        <a href="/logout" class="nav-link" 
-                            onclick="event.preventDefault();
+                        <a href="/logout" class="nav-link text-light ms-2" 
+                        onclick="event.preventDefault();
                             this.closest('form').submit();">
                             Sair
-                            <ion-icon name="arrow-forward-outline"></ion-icon>
+                            <i class="bi bi-box-arrow-right"></i>
                         </a>
                         
                     </form>
                 </div>
+
+                <div class="navbar-collapse collapse navbar-links">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link text-light btn btn-dark" href="/dashboard">Dashboard</a></li>
+                        <li class="nav-item"><a class="nav-link text-light btn btn-dark" href="/transactions">Transações</a></li>
+                        <li class="nav-item"><a class="nav-link text-light btn btn-dark" href="#">Relatório</a></li>
+                        <li class="nav-item"><a class="nav-link text-light btn btn-dark" href="#">Recorrências</a></li>
+                        <li class="nav-item"><a class="nav-link text-light btn btn-dark" href="#">Investimentos</a></li>
+                        <li class="nav-item"><a class="nav-link text-light btn btn-dark" href="#">Tutorial</a></li>
+                    </ul>
+                </div>
+                
             </div>
+            
         </nav>
     </header>
     <main>
@@ -53,7 +66,9 @@
         @yield('content')    
 
     </main>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/moneo/transactions.blade.php
+++ resources/views/moneo/transactions.blade.php
@@ -4,41 +4,46 @@
 
 @section('content')
 
-<div class="transactions-content">
+<div class="container-fluid p-5">
 
-    <h1>Transações</h1>
+    <h1 class="mb-4">Transações</h1>
 
-    <table>
+    <table class="table table-hover">
         <thead>
-            <tr>
-                <th class="column-date">Pagamento</th>
-                <th class="column-description">Descrição</th>
-                <th class="column-value">Valor</th>
-                <th class="column-category">Categoria</th>
-                <th class="column-type">Tipo</th>
-                <th class="column-payment">Metodo de Pagamento</th>
-                <th class="column-status">Status</th>
-                <th class="column-proof">Comprovante</th>
-                <th class="column-obs">Observações</th>
+            <tr class="table-secondary">
+                <th scope="col" class="col-lg-1">Data</th>
+                <th scope="col" class="col-lg-3 d-table-cell">Descrição</th>
+                <th scope="col" class="col-lg-1">Valor</th>
+                <th scope="col" class="col d-none d-md-table-cell">Categoria</th>
+                <th scope="col" class="col d-none d-sm-table-cell">Tipo</th>
+                <th scope="col" class="col d-none d-lg-table-cell">Pagamento</th>
+                <th scope="col" class="col-lg-1">Status</th>
+                <th scope="col" class="col-lg-1"><i class="bi bi-receipt text-dark d-lg-none"></i><span class="d-none d-lg-table-cell">Comprovante</span></th>
+                <th scope="col" class="col d-none d-lg-table-cell">Observações</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>29/08/2002</td>
-                <td>Aniversário do João</td>
-                <td>R$ 100.000,00</td>
-                <td>Aniversário, Festas, Presentes</td>
-                <td>Despesa</td>
-                <td>Cartão Inter</td>
-                <td>Pagando (2/10)</td>
-                <td>arquivo.jpg</td>
-                <td>Uma festa legal</td>
-            </tr>
+            @foreach($transactions as $transaction)
+                <tr>
+                    <td>{{ date( 'd/m/y' , strtotime($transaction->date)) }}</td>
+                    <td>{{ $transaction->description }}</td>
+                    <td>{{ $transaction->value }}</td>
+                    <td class="d-none d-md-table-cell">{{ $transaction->category }}</td>
+                    <td class="d-none d-sm-table-cell">{{ $transaction->type }}</td>
+                    <td class="d-none d-lg-table-cell">{{ $transaction->payment }}</td>
+                    <td>{{ $transaction->status }}</td>
+                    <td><a href="/proof/{{ $transaction->proof }}"><i class="bi bi-receipt"></i></a></td>
+                    <td class="d-none d-lg-table-cell">{{ $transaction->note }}</td>
+                </tr>
+            @endforeach
+            
 
         </tbody>
     </table>
 
-    <a href="/add-transaction" class="">Adicionar Transação</a>
+    
+
+    <a href="/add-transaction" class="btn btn-primary">Adicionar Transação</a>
 
 </div>
 
