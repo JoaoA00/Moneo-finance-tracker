b75d9bcfbd848e8f863def31e7f883e24b0ce317
João Abner <joaoabner2022@gmail.com>
2024-11-27 14:08:09 -0300

    Adicionando Bootstrap no projeto e removedo css desnecessário

--- resources/views/moneo/dashboard.blade.php
+++ resources/views/moneo/dashboard.blade.php
@@ -4,23 +4,81 @@
 
 @section('content')
 
-<div class="dashboard-content">
-    <div class="top-boxes">
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-    </div>
-    <div class="dashboard-data">
-        <div class="finalcial-graph">
-            
-        </div>
-        <div class="expenses">
-            <div class="recent-expenses">
-                
+<div class="container-fluid p-5">
+    <div class="card-group mb-5">
+        <div class="p-1 col-12 col-sm-6 col-md-3">
+            <div class="card p-0">
+                <div class="card-header py-1">
+                    Card header
+                </div>
+                <div class="card-body">
+                    
+                </div>
             </div>
-            <div class="fixed-expenses">
-                
+        </div>
+        <div class="p-1 col-12 col-sm-6 col-md-3">
+            <div class="card p-0">
+                <div class="card-header py-1">
+                    Card header
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="p-1 col-12 col-sm-6 col-md-3">
+            <div class="card p-0">
+                <div class="card-header py-1">
+                    Card header
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="p-1 col-12 col-sm-6 col-md-3">
+            <div class="card p-0">
+                <div class="card-header py-1">
+                    Card header
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="row gap-2 p-2">
+        <div class="col p-0 d-none d-sm-block"  style="height: 100%;">
+            <div class="col card p-0"  style="height: 100%;">
+                <div class="card-header py-1">
+                    Card header
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-12 p-0">
+            <div class="pb-2">
+                <div class="col card p-0">
+                    <div class="card-header py-1">
+                        Card header
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="">
+                <div class="col card p-0">
+                    <div class="card-header py-1">
+                        Card header
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,32 +4,48 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <!-- bootstrap-icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
         <title>Moneo</title>
-        <link rel="stylesheet" href="/css/landing-page-style.css">
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
         <header>
-            <nav>
-                <div class="header-itens">
-                    <div class="header-logo">
-                        <img src="/img/header-logo.png">
-                    </div>
-                    <div class="header-navigation">
-                        <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Documentation</a></li>
-                            <li><a href="#">Features</a></li>
+            <nav class="navbar navbar-expand-md p-3">
+                <div class="container-fluid d-flex">
+
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/header-logo.png" width="120" height="30">
+                    </a>
+
+                    <div class="d-flex order-md-3">
+                        <ul class="navbar-nav d-flex flex-row gap-2 gap-md-0 me-3 me-md-0">
+                            <li class="nav-item"><a href="/register" class="btn text-dark">Register</a></li>
+                            <li class="nav-item"><a href="/login" class="btn text-light" style="background-color: #D99F59;">Log in</a></li>                  
                         </ul>
-                    </div>
-                    <div class="login-register">
-                        <ul>
-                            <li><a href="/register" class="register-btn btn">Register</a></li>
-                            <li><a href="/login" class="login-btn btn">Log in</a></li>                  
-                        </ul>
+                        <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="bi bi-list h2"></span>
+                        </button>
                     </div>
                     
-                </div>
+                    <div class="navbar-collapse collapse" id="navbar-links">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link btn btn-hover-line" href="#">About</a></li>
+                            <li class="nav-item"><a class="nav-link btn btn-hover-line" href="#">Documentation</a></li>
+                            <li class="nav-item"><a class="nav-link btn btn-hover-line" href="#">Features</a></li>
+                        </ul>
+                    </div>
+
+                    
+                </div>   
             </nav>
         </header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous"></script>
     </body>
 </html>
